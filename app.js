88192f339027123b543d5c08f638086db11a45da
HarkLikeButter <92184835+HardLikeButter@users.noreply.github.com>
2022-03-30 16:59:17 -0400

    js updates

--- app.js
+++ app.js
@@ -2,20 +2,6 @@ const colorArr = ["green", "red", "Rgba(133, 122, 200)", "#f15025", "#00FFFF", "
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
-/*
-btn.addEventListener("click", function () {
-    // get random number between 0 - 3
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber);
-
-    document.body.style.backgroundColor = colorArr[randomNumber];
-    color.textContent = colorArr[randomNumber];
-});
-
-function getRandomNumber() {
-    return Math.floor(Math.random() * colorArr.length);
-}
-*/
 
 
 btn.addEventListener("click", () => {
