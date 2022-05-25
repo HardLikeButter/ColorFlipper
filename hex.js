64e22d3231ec1c6daa0ad4325983e7272ea4a3ed
HarkLikeButter <92184835+HardLikeButter@users.noreply.github.com>
2022-05-25 12:45:31 -0400

    Update

--- hex.js
+++ hex.js
@@ -4,19 +4,19 @@ const color = document.querySelector(".color");
 
 
 
-// btn.addEventListener("click", () => {
-//     let hexColor = "#";
-//     for (let i = 0; i < 6; i++) {
-//         hexColor += hex[getRandomNumber()];
-//     }
+btn.addEventListener("click", () => {
+    let hexColor = "#";
+    for (let i = 0; i < 6; i++) {
+        hexColor += hex[getRandomNumber()];
+    }
 
-//     color.textContent = hexColor;
-//     document.body.style.backgroundColor = hexColor;
-// });
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+});
 
 
-// function getRandomNumber() {
-//     return Math.floor(Math.random() * hex.length);
-// }
+function getRandomNumber() {
+    return Math.floor(Math.random() * hex.length);
+}
 
 
